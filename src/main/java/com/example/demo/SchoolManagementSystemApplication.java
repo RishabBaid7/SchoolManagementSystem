edac10a9e7d22412e2a2ec37275422a5a57c938c
Rishab Baid <rishabbaid@Rishabs-MacBook-Air.local>
2025-11-21 20:01:09 +0530

    added student model

--- src/main/java/com/example/demo/SchoolManagementSystemApplication.java
+++ src/main/java/com/example/demo/SchoolManagementSystemApplication.java
@@ -3,11 +3,14 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.example.demo.models.Student;
+
 @SpringBootApplication
 public class SchoolManagementSystemApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SchoolManagementSystemApplication.class, args);
+		// SpringApplication.run(SchoolManagementSystemApplication.class, args);
+		System.out.println(new Student().toString());
 	}
 
 }
---
+++ src/main/java/com/example/demo/models/Student.java
@@ -0,0 +1,50 @@
+package com.example.demo.models;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
+import org.hibernate.annotations.UuidGenerator;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "students")   
+public class Student {
+    @Id
+    @UuidGenerator
+    private UUID id;
+    
+    private UUID userId;
+
+    private String name;
+
+    private LocalDate dob;
+
+    private String gender;
+
+    @Column(name = "blood_group")
+    private String bloodGroup;
+
+    private String contact;
+
+    @Column(columnDefinition = "TEXT")
+    private String address;
+
+    @Column(name = "enrollment_class")
+    private String enrollmentClass;
+
+    private String section;
+
+    @Column(name = "roll_number")
+    private Integer rollNumber;
+
+    @Column(name = "emergency_contact")
+    private String emergencyContact;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt; 
+}
